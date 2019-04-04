e8855f0378a0aac3cdba115f6a27ddb8752cb436
Danny Parsons <42558542+dannyparsonscs@users.noreply.github.com>
2019-04-04 01:51:48 -0400

    Add files via upload
    These were in my htdocs folder when I set up xampp.

---
+++ survey/Survey.php
@@ -0,0 +1,207 @@
+<!DOCTYPE html>
+<html lang="en">
+    <style>
+        body
+        {
+          background-image: linear-gradient(60deg, #96deda 0%, #50c9c3 100%);
+        }
+        h1
+        {
+          color:#FFFFFF;
+          font-family:cursive;
+          text-shadow: -1px 0 black, 0 1px black, 1px 0 black, 0 -1px black;
+        }
+      
+        input[type=text]
+        {
+          border:2px solid black;
+          border-radius:4px;
+        }
+      
+        input[type=password]
+        {
+          border:2px solid black;
+          border-radius:4px;
+        }
+      
+        input[type=submit]
+        {
+          background-color:black;
+          font-family:cursive;
+          text-shadow: -1px 0 black, 0 1px black, 1px 0 black, 0 -1px black;
+          color:#F8F8FF;
+        }
+      
+        label
+        {
+          font-family:cursive;
+          color:#F8F8FF;
+          text-shadow: -1px 0 black, 0 1px black, 1px 0 black, 0 -1px black;
+        }
+      
+        h2
+        {
+          font-family:cursive;
+          color:black;
+          text-shadow: -1px 0 white, 0 1px white, 1px 0 white, 0 -1px white;
+        }
+      
+        button
+        {
+          background-color:black;
+          font-family:cursive;
+          text-shadow: -1px 0 black, 0 1px black, 1px 0 black, 0 -1px black;
+          color:#F8F8FF;
+        }
+      
+        fieldset
+        {
+          border-width:20px;
+          border-style:solid;
+          border-color:#6a7e81;
+          margin:auto;
+        }
+      
+        footer
+        {
+          font-size:75%;
+          font-style:italic;
+          font-family:georgia;
+          text-align:center;
+          padding-top: 135px;
+        }
+      
+        /* Home Page */
+        .home-page 
+        {
+          text-align: center;
+        }
+      
+        .home-page h1
+        {
+          color:#F8F8FF;
+          font-family:cursive;
+        }
+      
+        /* Login Page */
+        .login-page 
+        {
+          text-align: center;
+        }
+      
+        .login-page h1
+        {
+          color:#F8F8FF;
+          font-family:cursive;
+        }
+      
+        .field
+        {
+          width: 50%;
+          margin: auto;
+          padding-top: 20px;
+          padding-bottom: 10px;
+        }
+      
+        .button
+        {
+          padding-top: 10px;
+          padding-bottom: 10px;
+        }
+      
+        /* Register Page */
+        .register-page
+        {
+          text-align: center;
+        }
+      
+        .register-page h1
+        {
+          color:#F8F8FF;
+          font-family:cursive;
+        }
+      
+        .form-group
+        {
+          padding: 5px;
+        }
+      
+        .input
+        {
+          width: 50%;
+          height: 20px;
+        }
+      
+        .images
+        {
+          position:relative;
+          display:inline-block;
+        }
+      
+        .images p
+        {
+          position:relative;
+          width:100;
+          text-align:center;
+          left:0;
+          color:black;
+          font-family:cursive;
+        }
+      
+      </style>
+<head>
+  <meta charset="UTF-8">
+  <link rel="stylesheet" href="/CSS/Style.css" type="text/css">
+  <link rel="stylesheet" type="text/css" href="/CSS/Header.css">
+  <title>Survey</title>
+</head>
+
+<body>
+
+<div class="header">
+
+<div class="inner_header">
+
+<div class="logo_container">
+<h1><a href="Survey.php"><img src="../Images/DB.png" alt="" width="50"
+height="50"> <span>Survey</span><span>Master</span> </a></h1>
+ </div>
+<ul class="navigation">
+
+  <div class="dropdown">
+  <button class="dropbtn">Menu<i class="down"></i> </button> 
+
+  <div class="dropdown-content">
+  <a href="registration.php">Register</a>
+<a href="CreateSurvey.php">Create Survey</a>
+ <a href="Login.php">Logout</a>
+  </div>
+  </div>
+</ul>
+</div>
+</div>
+
+<div class="home-page">
+
+<form>
+  <h1>Survey</h1>
+
+  <p>Survey Description</p>
+  <br>
+
+
+  <p>Question 1</p>
+  <input type="radio" name="radio" value="1">1 <input type="radio" name="radio"
+  value="2">2 <input type="radio" name="radio" value="3">3 <input type="radio"
+  name="radio" value="4">4 <input type="radio" name="radio" value="5">5 <br>
+
+
+  <p>Question 2</p>
+  <textarea maxlength="200" rows="10" cols="50"></textarea><br>
+  <br>
+
+  <input type="submit" value="Submit"> </form>
+</div>
+</body>
+<footer>Copyright &copy; COP4710<br>
+</footer></html>
---
+++ survey/server.php
@@ -0,0 +1,53 @@
+<?php
+
+ session_start(); 
+
+// //initiatializing variables
+
+$username = ""; 
+$email = ""; 
+
+$errors = array(); 
+
+//Connect to DB
+
+$db = mysqli_connect('localhost', 'root', '', 'surveyusers') or die("Could not connect to database"); 
+
+// //Registering users
+$username = mysqli_real_escape_string($db, $_POST['username']); 
+$email = mysqli_real_escape_string($db, $_POST['email']); 
+$password1 = mysqli_real_escape_string($db, $_POST['password1']); 
+$password2 = mysqli_real_escape_string($db, $_POST['password2']); 
+
+//Form validation
+
+if(empty($username)) {array_push($errors, "Username is required");}
+if(empty($email)) {array_push($errors, "Email is required");}
+if(empty($password1)) {array_push($errors, "Password is required");}
+if($password1 != $password2) {array_push($errors, "Passwords do not match");}
+
+//check database for existing user with same username
+$user_check_query = "SELECT * FROM user WHERE username = '$username' or email = '$email' LIMIT 1";
+
+$result = mysqli_query($db, $user_check_query); 
+$user = mysqli_fetch_assoc($result); 
+
+if($user)
+{
+    if($user['username'] === $username) {array_push($errors, "Username already exists.");}
+    if($user['email'] === $email) {array_push($errors, "Email already in use.");}
+}
+
+// REgister user if no errors
+if(count($errors) == 0)
+{
+    $passwords = md5($password1); //encrypt password
+    $query = "INSERT INTO user (username, email, pass) VALUES ('$username', '$email', '$passwords')";
+    mysqli_query($db, $query); 
+    $_SESSION['username'] = $username; 
+    $_SESSION['success'] = "Login successful.";
+    
+    header("location: CreateSurvey.php"); 
+}
+
+?>
